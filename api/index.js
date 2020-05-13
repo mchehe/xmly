f848cd42a9018b02dc909c459a402a91c4c7830e
mchehe <2596844541@qq.com>
2020-05-14 01:50:42 +0800

    d

--- api/index.js
+++ api/index.js
@@ -159,7 +159,24 @@ function xm_md5(){
 };
 
 module.exports = (req, res) => {
-	var ok='{"ret":0,"msg":"0","trackId":227985666,"uid":134944915,"albumId":30736201,"title":"都市最强仙帝027集阴鬼宗再现","domain":"http://audiopay.cos.xmcdn.com","totalLength":10274310,"sampleDuration":180,"sampleLength":1562130,"isAuthorized":true,"apiVersion":"1.0.0","seed":6367,"fileId":"18*13*43*50*31*61*59*60*67*26*59*53*5*59*65*48*59*24*55*18*60*6*48*61*51*3*49*62*18*14*47*56*7*53*9*21*1*40*46*64*43*39*64*60*67*0*44*16*30*38*41*","buyKey":"617574686f72697a6564","duration":1269,"ep":"20NvOoh6T39X3qwKO4cY5g5bVhg+hSPOT4QaeAq3WCurneyLjf7aybgHg/yJ3vIzCOUkgScYe6di3uz+xg0c2LYTPiBS","highestQualityLevel":1,"downloadQualityLevel":1,"authorizedType":1}';
-	var ew= JSON.parse(ok);
-	res.send(get_player_url(ew));
+	xm_md5().then(function(xm_md5){
+        var url = 'https://mpay.ximalaya.com/mobile/track/pay/227985666?device=pc&isBackend=true&_='+Date.now();
+        var cookie = '_xmLog=xm_k9a0xy9ysj4k4p; 1&remember_me=y; 1&_token=204709552&31B92E42A4F8446E85B226EE5A7694ADNdV09F1DCB6A5BC53204EDE97232C21B9A13437F7812B40A8A593C328954BA55030; x_xmly_traffic=utm_source%253A%2526utm_medium%253A%2526utm_campaign%253A%2526utm_content%253A%2526utm_term%253A%2526utm_from%253A; Hm_lvt_4a7d8ec50cfd6af753c4f8aee3425070=1588318846,1588400664,1588573395,1589352175; Hm_lpvt_4a7d8ec50cfd6af753c4f8aee3425070=1589352571';
+        request({
+            url: url,
+            method: "GET",
+            json: true,
+            headers: {
+                "Host": "mpay.ximalaya.com",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+                "xm-sign": xm_md5,
+                "cookie": cookie,
+                "Referer": "https://www.ximalaya.com/xiangsheng/5932959/25620080"              
+            },
+        }, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                res.send(get_player_url(body));
+            }
+        });
+    });
 }
